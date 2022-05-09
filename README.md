0de85aa1f9de01d0d73f83106f3c515e7fac616c
jeremy <74825770+jerepe@users.noreply.github.com>
2022-05-09 19:49:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Len's Slice
+
+### What does it do ?
+> Prints few list, getting updated as the program progresses. Lists record the buying/selling activity going on in a pizzeria named "Len's Slice"
+
+### How does it looks like ?
+> ![lensslice](pic.png)
+
+### How do i use it ?
+> Just run it using Python.
+
+### How was it done ?
+> Using CLI, Emacs, and Python
+
+### Why ?
+> CodeCademy project that i reformatted for training purposes.
+> Done to train myself on lists
---
+++ lensslice.py
@@ -0,0 +1,39 @@
+# Let's talk about pizzas:
+
+# toppings' list:
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+# slices costs:
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+# how many 2$ slices?
+num_two_dollar_slices = prices.count(2)
+
+# toppings lenght:
+num_pizzas = len(toppings)
+
+# advertise:
+print()
+print("We sell " + str(num_pizzas) + " different kinds of pizza! Here they are, sorted by ascending price:" + "\n")
+
+# creating 2D list so we can sort them by price later on:
+pizza_and_prices = [[2, "pepperoni"],[6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+pizza_and_prices.sort()
+print(pizza_and_prices)
+
+# identifying cheapest pizza:
+cheapest_pizza = pizza_and_prices[0]
+
+# someone comes and buys the las piece of the  most expensive pizza, lets identify it to later remove it:
+priciest_pizza = pizza_and_prices[-1]
+
+# removing it:
+pizza_and_prices.pop()
+
+# adding new topping, respecting proper sorting order:
+pizza_and_prices.insert(4, [2.5, "peppers"])
+print("\n" + "Someone just bought the last piece of the cheese pizza, but no worries, we have added another one:" + "\n" + "\n" + str(pizza_and_prices))
+
+# storing pizza slices for mice:
+three_cheapest = pizza_and_prices[0:3]
+print("\n" + "Ahhhhhhh .... actually today we give the following ones  to a few mice sniffing around: " + "\n" + "\n" + str(three_cheapest) + "\n")
